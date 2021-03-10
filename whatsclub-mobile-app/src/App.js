d068c7043a42cb2b24dff587118abfab79a99d35
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2021-03-10 22:00:14 +0300

    Add react native app

---
+++ whatsclub-mobile-app/src/App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+} from 'react-native';
+
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+    return (
+        <>
+            <Text>Whatsclub</Text>
+        </>
+    );
+};
+
+export default App;
