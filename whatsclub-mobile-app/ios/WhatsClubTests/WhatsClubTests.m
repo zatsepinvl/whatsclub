92cc2b80ade08359479fa245ff24daf2b0fae321
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2021-03-11 20:40:47 +0300

    Rename mobile app folder

--- whatsclub-mobile-app/ios/WhatsClubTests/WhatsClubTests.m
+++ whatsclub-mobile-app/ios/WhatsClubTests/WhatsClubTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WhatsclubTests : XCTestCase
+@interface WhatsClubTests : XCTestCase
 
 @end
 
-@implementation WhatsclubTests
+@implementation WhatsClubTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
