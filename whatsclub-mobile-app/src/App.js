54f5f913117bdd02365bdf0830fb076233011289
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2021-03-11 20:41:11 +0300

    Make initial changes

--- whatsclub-mobile-app/src/App.js
+++ whatsclub-mobile-app/src/App.js
@@ -1,34 +1,45 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  * @flow strict-local
  */
+import React, {useEffect, useState} from 'react';
+import { ActivityIndicator, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
-import React from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    StatusBar,
-} from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    horizontal: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10
+    }
+});
 
-import {
-    Header,
-    LearnMoreLinks,
-    Colors,
-    DebugInstructions,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
+
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://reactnative.dev/movies.json')
+            .then((response) => response.json())
+            .then((json) => setData(json.movies))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
     return (
-        <>
-            <Text>Whatsclub</Text>
-        </>
+        <SafeAreaView style={[styles.container]}>
+            <View style={[styles.horizontal]}>
+                <ActivityIndicator />
+                <ActivityIndicator size="large" />
+                <ActivityIndicator size="small" color="#000000" />
+                <ActivityIndicator size="large" color="blue" />
+            </View>
+        </SafeAreaView>
     );
 };
 
